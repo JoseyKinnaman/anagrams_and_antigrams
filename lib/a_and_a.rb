ea23ea198a2dd55b87cf632341deeffbc85585e9
Jozy Kinnaman <jozypants@gmail.com>
2020-05-08 13:57:08 -0700

    refactor is_word? and run test.

--- lib/a_and_a.rb
+++ lib/a_and_a.rb
@@ -6,16 +6,6 @@ class AAA
     @word2 = word2
   end
 
-   def is_word?
-    first_word = @word1
-    sec_word = @word2
-    if first_word.count('aeiouy') >= 1 && sec_word.count('aeiouy') >= 1 
-      return true
-    else 
-      return "You need to input an actual word"
-    end
-  end
-
   def anagrams?
     choice1 = @word1.downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').split("").sort
     choice2 = @word2.downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').split("").sort
@@ -24,9 +14,16 @@ class AAA
    else
     "These words are not anagrams"
     end
-    binding.pry
   end
 
- 
+  def is_word?
+    first_word = @word1.downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').split("")
+    sec_word = @word2.downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').split("")
+    if first_word.all?{ |word| word.count('aeiouy') >= 1} && sec_word.all?{ |word| word.count('aeiouy') >= 1} 
+    else 
+    "You need to input an actual word"
+    end
+  binding.pry
+  end
 end
 
--- spec/a_and_spec.rb
+++ spec/a_and_spec.rb
@@ -23,7 +23,7 @@ end
 
 describe("AAA#is_word?") do 
   it("checks if inputs are words") do
-    words = AAA.new("yurb", "Ruby")
-    expect(words.is_word?()).to(eq(true))
+    words = AAA.new("ruby", "Ruby")
+    expect(words.is_word?).to(eq(true))
   end
 end
