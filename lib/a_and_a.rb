704af71d85d350f04c716c234577efa70636eec0
Jozy Kinnaman <jozypants@gmail.com>
2020-05-08 14:57:47 -0700

    refactored instance variables.

--- lib/a_and_a.rb
+++ lib/a_and_a.rb
@@ -2,13 +2,13 @@ require ('pry')
 
 class AAA
   def  initialize(word1, word2)
-    @word1 = word1
-    @word2 = word2
+    @word1 = word1.downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
+    @word2 = word2.downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '')
   end
 
   def anagrams?
-    choice1 = @word1.downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').split("").sort
-    choice2 = @word2.downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').split("").sort
+    choice1 = @word1.split("").sort
+    choice2 = @word2.split("").sort
    if (choice1 == choice2) 
     "These words are anagrams."
    else
@@ -17,8 +17,8 @@ class AAA
   end
 
   def is_word?
-    first_word = @word1.downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').split(" ")
-    sec_word = @word2.downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').split(" ")
+    first_word = @word1.split(" ")
+    sec_word = @word2.split(" ")
     if first_word.all?{ |str| str.count('aeiouy') >= 1} && sec_word.all?{ |str| str.count('aeiouy') >= 1} 
       true
     else 
@@ -27,15 +27,14 @@ class AAA
   end
 
   def is_antigram?
-    input1 = @word1.downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').split("").sort
-    input2 = @word2.downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').split("").sort
+    input1 = @word1.split("").sort
+    input2 = @word2.split("").sort
     if
       input1.any?{|input1| input2.include?(input1)}
       false
     else
       true
     end
-    binding.pry
   end
 end
 
--- spec/a_and_spec.rb
+++ spec/a_and_spec.rb
@@ -31,6 +31,6 @@ end
 describe("AAA#is_antigram?") do
   it("checks if inputs aren't anagrams, then the method should check whether they are actually 'antigrams.'") do
     words = AAA.new("eat", "tea")
-    expect(words.is_antigram?).to(eq(true))
+    expect(words.is_antigram?).to(eq(false))
   end
 end
